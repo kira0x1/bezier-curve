28b5389d3426a9d9c93dc677f635d5269c40d8b5
Kira <kira.eva@protonmail.com>
2024-05-29 19:28:10 +0100

    display control points

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import styles from "./App.module.css";
 import DrawCurve from "./curve/drawCurve";
 
 function App() {
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>CURVE</header>
-      <content class={styles.content}></content>
-      <DrawCurve />
-    </div>
-  );
+	return (
+		<div class={styles.App}>
+			<header class={styles.header}>CURVE</header>
+			<content class={styles.content}>
+				<DrawCurve />
+			</content>
+		</div>
+	);
 }
 
 export default App;
--- src/curve/drawCurve.jsx
+++ src/curve/drawCurve.jsx
@@ -2,58 +2,83 @@ import styles from "../App.module.css";
 import bezierCurve from "./bezierCurve";
 
 const DrawCurve = () => {
-    const startPoint = { x: 0, y: 0 };
-    const controlPoint1 = { x: 100, y: 50 };
-    const controlPoint2 = { x: 200, y: 150 };
-    const endPoint = { x: 300, y: 200 };
-    const numPoints = 10;
+	const startPoint = { x: 10, y: 10 };
+	const controlPoint1 = { x: 110, y: 50 };
+	const controlPoint2 = { x: 200, y: 150 };
+	const endPoint = { x: 300, y: 200 };
+	const numPoints = 10;
 
-    const curvePoints = bezierCurve(startPoint, controlPoint1, controlPoint2, endPoint, numPoints);
+	const curvePoints = bezierCurve(startPoint, controlPoint1, controlPoint2, endPoint, numPoints);
 
-    return (
-        <>
-            <div class={styles.pointsMap}>
-                {RenderCurve(curvePoints)}
-            </div>
-        </>
-    );
+	return (
+		<>
+			<div class={styles.pointContainer}>
+				<svg width='800' height='600'>
+					{RenderControlPoint(controlPoint1, "1")}
+					{RenderControlPoint(controlPoint2, "2")}
+					{RenderCurve(curvePoints)}
+				</svg>
+			</div>
+		</>
+	);
+};
+
+const RenderControlPoint = (controlPoint, title) => {
+	const { x, y } = controlPoint;
+	return (
+		<>
+			<rect
+				width='15'
+				height='15'
+				x={x + 30}
+				y={y - 40}
+				rx='10'
+				ry='10'
+				style='fill:rgb(255,255,255,0.3);stroke:white;stroke-width:3;opacity:0.6'
+			/>
+			<div class={styles.pointIndex}>{title}</div>
+			<div class={styles.point}>{`( ${x}, ${y} )`}</div>
+		</>
+	);
 };
 
 const RenderCurve = (curvePoints) => {
-    const lines = [];
+	const lines = [];
 
-    for (let i = 0; i < curvePoints.length - 1; i++) {
-        const point1 = curvePoints[i];
-        const point2 = curvePoints[i + 1];
+	for (let i = 0; i < curvePoints.length - 1; i++) {
+		const point1 = curvePoints[i];
+		const point2 = curvePoints[i + 1];
 
-        lines.push(
-            <>
-                <circle key={i} cx={point1.x} cy={point1.y} r="1" fill="white" />
-                <circle key={i + 1} cx={point2.x} cy={point2.y} r="3" fill="magenta" />
-                <line x1={point1.x} y1={point1.y} x2={point2.x} y2={point2.y} stroke="white" />
-            </>
-        );
-    }
+		lines.push(
+			<>
+				<line x1={point1.x} y1={point1.y} x2={point2.x} y2={point2.y} stroke='#Cf509F' stroke-width='3px' />
+				{i > 0 && <circle key={i} cx={point1.x} cy={point1.y} r='3' fill='white' />}
+				{/* <circle key={i + 1} cx={point2.x} cy={point2.y} r='3' fill='white' /> */}
+			</>
+		);
+	}
 
-    return (
-        <svg width="400" height="400">
-            {lines}
-        </svg>
-    );
+	lines.push(createPoint(curvePoints[0]));
+
+	return lines;
+};
+
+const createPoint = (point) => {
+	return <circle cx={point.x} cy={point.y} r='3' fill='white' />;
 };
 
 const curveValuesDebug = (curvePoints) => {
-    return (
-        <div class={styles.pointsMap}>
-            {curvePoints.map((c, i) => {
-                return (
-                    <div class={styles.pointContainer}>
-                        <div class={styles.pointIndex}>{i}</div>
-                        <div class={styles.point}>{`( ${c.x.toFixed(1)}, ${c.y.toFixed(1)} )`}</div>
-                    </div>
-                );
-            })}
-        </div>
-    );
+	return (
+		<div class={styles.pointsMap}>
+			{curvePoints.map((c, i) => {
+				return (
+					<div class={styles.pointContainer}>
+						<div class={styles.pointIndex}>{i}</div>
+						<div class={styles.point}>{`( ${c.x.toFixed(1)}, ${c.y.toFixed(1)} )`}</div>
+					</div>
+				);
+			})}
+		</div>
+	);
 };
 export default DrawCurve;
