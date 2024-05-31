d87d1adb9c12f69d93fa76a1718666492bc3ca38
Kira <kira.eva@protonmail.com>
2024-05-31 17:06:45 +0100

    refactor control points

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import styles from "./App.module.css";
-import DrawCurve from "./curve/drawCurve";
+import styles from "./styles/App.module.css";
+import DrawCurve from "./curve/BezierCurve";
 
 function App() {
-	return (
-		<div class={styles.App}>
-			<header class={styles.header}>CURVE</header>
-			<content class={styles.content}>
-				<DrawCurve />
-			</content>
-		</div>
-	);
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>CURVE</header>
+      <content class={styles.content}>
+        <DrawCurve />
+      </content>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/curve/utils.jsx
@@ -0,0 +1,28 @@
+// Define the Bezier curve function
+function bezierCurve(startPoint, controlPoint1, controlPoint2, endPoint, numPoints) {
+  const points = [];
+
+  for (let t = 0; t <= 1; t += 1 / numPoints) {
+    const x =
+      Math.pow(1 - t, 3) * startPoint.x +
+      3 * Math.pow(1 - t, 2) * t * controlPoint1.x +
+      3 * (1 - t) * Math.pow(t, 2) * controlPoint2.x +
+      Math.pow(t, 3) * endPoint.x;
+
+    const y =
+      Math.pow(1 - t, 3) * startPoint.y +
+      3 * Math.pow(1 - t, 2) * t * controlPoint1.y +
+      3 * (1 - t) * Math.pow(t, 2) * controlPoint2.y +
+      Math.pow(t, 3) * endPoint.y;
+
+    points.push({ x, y });
+  }
+
+  return points;
+}
+
+const Utils = {
+  bezierCurve: bezierCurve,
+};
+
+export default Utils;
